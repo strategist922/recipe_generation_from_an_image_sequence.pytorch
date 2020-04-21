64697d29858b3bb0d21b95c350c4228afba28d06
misogil0116 <lack_un@yahoo.co.jp>
2020-04-21 10:17:49 +0900

    fix

--- preprocess/build_dataset.py
+++ preprocess/build_dataset.py
@@ -15,7 +15,7 @@ random.seed(42)
 
 def remove_introduction(context):
     if "introduction" in context[0]["step_title"].lower():
-        return context[1:]
+            return context[1:]      
     else:
         return context
 
@@ -26,6 +26,10 @@ def load_and_extract_recipes(filename):
     for recipe in recipes:
         context = recipe["context"]
         perfectly_aligned = True
+
+        if len(context) == 0:
+            continue
+
         context = remove_introduction(context)
         for step_content in context:
             if len(step_content["step_images"]) == 0:
@@ -34,6 +38,16 @@ def load_and_extract_recipes(filename):
             recipe["context"] = context
             aligned_recipes.append(recipe)      
     return aligned_recipes
+
+def download_images(image_dir, recipes):
+    for recipe in recipes:
+        context = recipe["context"]
+        for step in context:
+            images = step["step_images"]
+            for image in images:
+                url, filename = image[0], image[1]
+                filename = os.path.join(image_dir, filename)
+                os.system("wget -O "+ filename + " " + url)
 
 def pick_up_one_image():
     pass
@@ -49,22 +63,25 @@ def build_dataset(args):
     # snapguide.com
     snapguide_filename = os.path.join(args.directory, "snapguide.json")
     snapguide_recipes = load_and_extract_recipes(snapguide_filename)
+    #import ipdb; ipdb.set_trace()
 
     # download images
     recipes = instructable_recipes + snapguide_recipes
-    download_images(recipes)
+    if args.dl:
+        image_dir = os.path.join(args.directory, "images")
+        download_images(image_dir, recipes)
 
+    # [WIP]
     # Split them into 8:1:1
-    
     
     # Building vocabulary with training data
 
     # Saving training, val, test, and vocabulary data.
-    dump_to_pickle()
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--directory", "-d", required=True, help="directory of dumpled json files via story_boarding_of_a_recipe")
+    parser.add_argument("--dl", action='store_true', help="True if you want to download")
     args = parser.parse_args()
     build_dataset(args)
